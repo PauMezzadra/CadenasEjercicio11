c6fb995d8d76f9a2382a3162634689e37ed21af0
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-25 16:50:10 -0300

    Add project files.

---
+++ CadenasEjercicio11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CadenasEjercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadenaConTildes = "";
+
+            do
+            {
+                Console.WriteLine("Ingrese una palabra o frase que contenga tildes");
+                cadenaConTildes = Console.ReadLine();
+                if (cadenaConTildes == "")
+                {
+                    Console.WriteLine("No puede ser vacío");
+                }
+                else
+                {
+                    Regex reg = new Regex("[^a-zA-Z0-9 ]");
+                    string textoNormalizado = cadenaConTildes.Normalize(NormalizationForm.FormD);
+                    string textoSinAcentos = reg.Replace(textoNormalizado, "");
+                    Console.WriteLine($"El texto sin tildes es: {textoSinAcentos}");
+                }
+
+            } while (cadenaConTildes == "");
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
